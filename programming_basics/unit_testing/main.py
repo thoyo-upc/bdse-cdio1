043cd61137889c153a97cb502cc5a775ef404159
Toni M. del Hoyo <antoni.martinez.del.hoyo@upc.edu>
2026-08-18 09:28:19 +0200

    Organize in folders

---
+++ programming_basics/unit_testing/main.py
@@ -0,0 +1,13 @@
+def sum_values(a, b):
+    return a + b
+
+
+def main():
+    x = 5
+    y = 3
+    result = sum_values(x, y)
+    print(f"The sum of {x} and {y} is {result}")
+
+
+if __name__ == "__main__":
+    main()
